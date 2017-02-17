ee80062ac58fd3aa10896b5f10edb206f36aaa50
luizlucasi <luizlucasi@gmail.com>
2017-02-17 15:57:02 -0200

    Added Junit

---
+++ src/test/java/com/lucasi/webapp/TestApp1.java
@@ -0,0 +1,14 @@
+package com.lucasi.webapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestApp1 {
+
+	@Test
+	public void test() {
+		assertTrue(1 + 1 == 2);
+	}
+
+}
